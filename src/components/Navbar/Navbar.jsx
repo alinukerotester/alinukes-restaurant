4211fc78bfe5db99a36a5a6ac2b4e03ea0df4ed1
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-08-14 13:02:02 +0300

    cateva modificari

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => <nav className="app__navbar">Navbar</nav>;
+const Navbar = () => (
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="logo" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#about">About</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#menu">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#awards">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navbar;
